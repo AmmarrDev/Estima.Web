84974fb24c75827a11766cc312dde03b944fab1f
Wajiha_Abid <Wajiha.Abid@CSquare.co>
2023-10-07 23:13:49 +0500

    Add project files.

---
+++ Estima.Web/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Estima.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Estima.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Expertise()
+        {
+            return View();
+        }
+
+        public IActionResult  ProjectTypes()
+        {
+            return View();
+        }
+
+        public IActionResult SoftwareExcellence()
+        {
+            return View();
+        }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public IActionResult Commitment()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ContactForm()
+        {
+            return View("ContactForm");
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Estima.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Estima.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
